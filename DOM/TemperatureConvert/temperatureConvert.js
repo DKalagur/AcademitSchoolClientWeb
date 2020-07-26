8d7c76a1a324220795ac999190cf4acff2029351
Денис Калагур <kalagurde@gmail.com>
2020-07-26 21:00:16 +0700

    V2

---
+++ DOM/TemperatureConvert/temperatureConvert.js
@@ -0,0 +1,51 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var convertButton = document.getElementById("convert-button");
+    var temperatureTextField = document.getElementById("temperature");
+
+    function convert() {
+        var text = temperatureTextField.value;
+
+        if (isNaN(Number(text)) || text.trim().length === 0) {
+            alert("Необходимо ввести число. Повторите ввод");
+            temperatureTextField.value = "";
+        } else {
+            //создаем текст с информацией о конвертированной температуре
+            var convertToKelvinText = document.createElement("p");
+            convertToKelvinText.textContent = "Температура по шкале Кельвина: " + (parseFloat(text) + 273.15).toFixed(2);
+
+            var convertToFahrenheitText = document.createElement("p");
+            convertToFahrenheitText.textContent = "Температура по шкале Фаренгейта: " + (parseFloat(text) * 9 / 5 + 32).toFixed(2);
+
+            temperatureTextField.value = "";
+
+            var convertToKelvinProposition = document.getElementById("convertToKelvinProposition");
+            var convertToFahrenheitProposition = document.getElementById("convertToFahrenheitProposition");
+
+            //если текстовых полей еще нет (первый ввод): создаем новые поля и присваим этим полям id. Если поля уже есть, то обновляем в них информацию
+            if (convertToKelvinProposition == null) {
+                convertToKelvinProposition = document.body.appendChild(convertToKelvinText);
+                convertToKelvinProposition.id = "convertToKelvinProposition";
+            } else {
+                convertToKelvinProposition.textContent = convertToKelvinText.textContent;
+            }
+
+            if (convertToFahrenheitProposition === null) {
+                convertToFahrenheitProposition = document.body.appendChild(convertToFahrenheitText);
+                convertToFahrenheitProposition.id = "convertToFahrenheitProposition";
+            } else {
+                convertToFahrenheitProposition.textContent = convertToFahrenheitText.textContent;
+            }
+            convertToFahrenheitProposition.setAttribute("class", "convert");
+            convertToKelvinProposition.setAttribute("class", "convert");
+        }
+    }
+
+    convertButton.addEventListener("click", convert);
+
+    temperatureTextField.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            convert();
+            e.preventDefault();
+        }
+    });
+});
