e8b45a6d669caec65881426e28a225f9e467ad26
Денис Калагур <kalagurde@gmail.com>
2020-08-02 14:57:49 +0700

    Task PhoneNumber V2

--- JQuery/PhoneBook/phoneBook.js
+++ JQuery/PhoneBook/phoneBook.js
@@ -1,63 +1,165 @@
 $(document).ready(function () {
-    var addButton = $("add-new-people");
-
+    var errorListBlock = $("#error-list-block");
+    // навешиваем обработчик на кнопку "Добавить запись"
     $("#add-new-people").click(function () {
-        var errorList = $("#error-list");
-        $("li").remove();
-        $("#all-list").hide();
-        $("input").removeClass("emptyField");
-        var error = false;
-        var values = [];
-        var emptyFieldsName = [];
-        var number = $(".number").length;
+        // очищаем старую информацию и стили
+        $("#error-list li").remove();
+        errorListBlock.hide();
+        $(".block-right input").removeClass("empty-field");
 
-        $("input").each(function (index) {
-            values.push($(this).val());
-            if ($.trim($(this).val()).length === 0) {
-                //кладем в список название незаполненных полей
-                emptyFieldsName.push($(this).closest('label').text());
-                $(this).addClass("emptyField");
-                error = true;
+        // блок проверки, что контакт с таким номером уже существует
+        var samePhoneIsExistError = false;
+        var inputPhone = $("#form-phone").val();
+
+        $(".phone").each(function () {
+            if ($(this).text() === inputPhone) {
+                samePhoneIsExistError = true;
             }
         });
 
-        if (error === true) {
-            //показываем блок про ошибку и отображаем названия незаполненных полей
-            $("#all-list").show();
-            for (var i = 0; i < emptyFieldsName.length; ++i) {
-                var emptyFieldName = $("<li></li>").addClass("error");
-                emptyFieldName.text(emptyFieldsName[i]);
-                errorList.append(emptyFieldName);
+        // блок проверки на незаполненные поля
+        var notAllFieldsEnteredError = false;
+        var inputFieldsValues = []; // заводим массив, в которой будем хранить введенные значения
+        var emptyFieldsName = [];
+
+        $(".block-right input:not(:checkbox)").each(function () {
+            inputFieldsValues.push($(this).val());
+            if ($.trim($(this).val()).length === 0) {
+                // кладем в массив название незаполненных полей
+                emptyFieldsName.push($(this).closest('label').text());
+                $(this).addClass("empty-field");
+                notAllFieldsEnteredError = true;
             }
+        });
+
+        if (notAllFieldsEnteredError) {
+            errorListBlock.show();
+            emptyFieldsName.forEach(function (element) {
+                $("#error-list").append($("<li></li>").text(element)).addClass("error");
+            })
+        } else if (samePhoneIsExistError) {
+            $.alert({
+                title: 'Ошибка!',
+                content: 'Контакт с таким телефоном уже существует!',
+                boxWidth: '300px',
+                useBootstrap: false,
+            });
         } else {
-            //формируем строку для добавления в таблицу в контактами
+            // формируем строку для добавления в таблицу в контактами
             var newLine = $("<tr></tr>");
 
-            //добавляем поле-номер
-            var cell = $("<td class='number'></td>");
-            cell.text(number + 1);
-            newLine.append(cell);
+            // добавляем поле-чек бокс
+            newLine.append($("<td class='notInfoCell'><input type='checkbox' class='check'></td>"));
 
-            // добавляем остальные поля (кроме кнопки "удалить")
-            $(values).each(function (index, element) {
-                cell = $("<td></td>");
-                cell.text(element);
-                newLine.append(cell);
+            // добавляем поле-номер
+            newLine.append($("<td class='number'></td>").text($(".number").length + 1));
+
+            // добавляем информативные поля
+            var fieldClasses = ["name", "surname", "phone"];
+            $(inputFieldsValues).each(function (index, element) {
+                $("<td></td>")
+                    .text(element)
+                    .addClass(fieldClasses[index])
+                    .addClass("infoCell")
+                    .appendTo(newLine);
             });
 
             // добавляем кнопку "удалить"
-            newLine.append("<td><button type='button' class ='close'>X</button></td>");
+            newLine.append("<td class='notInfoCell'><button type='button' class ='close'>X</button></td>");
 
             $("#tbody").append(newLine);
 
-            //навешиваем обработчик на кнопку "удалить"
-            $(".close").click(function () {
-                $(this).closest("tr").remove();
-                $(".number").each(function (i) {
-                    $(this).text(i + 1);
+            // навешиваем обработчик на кнопку "удалить"
+            $(".close:last").click(function () {
+                var self = $(this);
+
+                // выводим сообщение-confirmation
+                $.confirm({
+                    title: 'Предупреждение',
+                    content: 'Вы уверены?',
+                    boxWidth: '300px',
+                    useBootstrap: false,
+                    buttons: {
+                        yes: function () {
+                            self.closest("tr").remove();
+                            recalculateNumbersAfterDel();
+                        },
+                        cancel: function () {
+                        },
+                    }
                 });
             });
             $("input").val("");
         }
+    });
+
+    function recalculateNumbersAfterDel() {
+        $(".number").each(function (i) {
+            $(this).text(i + 1);
+        });
+    }
+
+    // навешиваем обработчик на чек-бокс в шапке
+    $("#main-checkbox").change(function () {
+        if ($(this).is(':checked')) {
+            $(".check").prop('checked', true);
+        } else {
+            $(".check").prop('checked', false);
+        }
+    });
+
+    // навешиваем обработчик на кнопку "Удалить выделенное"
+    $("#del-selected-row").click(function () {
+        // confirmation-form
+        $.confirm({
+            title: 'Предупреждение',
+            content: 'Вы уверены?',
+            boxWidth: '300px',
+            useBootstrap: false,
+            buttons: {
+                yes: function () {
+                    // удаляем все выделенные строки (кроме шапки таблицы)
+                    $(":checked:not(#main-checkbox)").closest("tr").remove();
+                    // скидываем чекбокс в шапке таблицы
+                    $("#main-checkbox").prop('checked', false);
+                    // после удаления строки пересчитываем номера оставшихся строк
+                    recalculateNumbersAfterDel();
+                },
+                cancel: function () {
+                },
+            }
+        });
+    });
+
+    function search() {
+        var substring = $("#search").val();
+
+        // находим ячейки таблицы, текст которых содержит искомую подстроку
+        var includingSubstringCells = $(".infoCell").filter(function () {
+            var self = $(this);
+            return self.text().includes(substring);
+        });
+
+        var bodyTr = $("tbody tr");
+
+        bodyTr.not($("tr").has(includingSubstringCells)).hide();
+        bodyTr.has(includingSubstringCells).show();
+    }
+
+    // навешиваем обработчик на кнопку "Поиск"
+    $("#do-search").click(search());
+
+    // навешиваем обработчик на ввод в поле "Поиск"
+    $('#search').keydown(function (e) {
+        if (e.key === "Enter") {
+            search();
+            e.preventDefault();
+        }
+    });
+
+    // навешиваем обработчик на кнопку "Отмена поиска"
+    $("#cancel-search").click(function () {
+        $("tr").show();
+        $("#search").val("");
     });
 });
